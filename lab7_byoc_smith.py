c19dc0c953472adc0c40b5dad820b86cb89bf23f
Celine Smith <cnsmith13@alaska.edu>
2026-03-02 15:54:02 -0900

    now can plot traces for a single section. Will combine with gps coordinates and allow for multiple sections to be read

--- lab7_byoc_smith.py
+++ lab7_byoc_smith.py
@@ -1,25 +1,45 @@
 '''
 This is a script that opens and reads SEG-Y format files downloaded from 
-NOAA NCEI. It plots the traces in the segy files and combines them with
-the gps coordinates in associated .txt files.
+NOAA NCEI. It converts seg-y traces to a 2-D numpy array and plots them.
+
+Not yet completed (##): combining traces with gps coordinates. Reading in 
+multiple files and concatenating them to view larger areas of trackline.
 
 inputs: seg-y file, associated .txt file containing gps coordinates of pings
-outputs: plot of twt.
+outputs: plot of traces
 
 '''
 
 import numpy as np
 import matplotlib.pyplot as plt
 from obspy.io.segy.segy import _read_segy
-from obspy.core.util import get_example_file
 import sys
 
+# import parameters
 
-segy = _read_segy("/Users/celinesmith/Desktop/GEOS694/assignments/GEOS694_lab7byoc_smith/HE0703/Digital/he0703_2007_229_1847_LF_000.sgy")
+filepath = sys.argv [1]
+##textfile = sys.argv [2]
 
-print(segy)
+# read in segy file
+
+segy = _read_segy(filepath, headonly = True)
+
+# creating 2-D numpy array from traces held in segy object
+# .T transposes so that twt is y-axis and trace number is x-axis
+
+data = np.stack([t.data for t in segy.traces]).T
+
+# printing rows and columns
+
+print(f"array shape is: {data.shape}")
 
 
+# plotting stacked data. TO UPDATE: find a better cmap for visualization
 
-# def segy_to_numpy():
-#     '''turns binary data into a 2D numpy array for plotting'''
+plt.imshow(data, cmap='gray_r', aspect='auto')
+
+plt.title("Healy 0703_2007_229_1847 Subbottom")
+plt.xlabel("trace number")
+plt.ylabel("two way travel time")
+plt.colorbar(label="amplitude")
+plt.show()
